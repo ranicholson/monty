338fbf1ec27fcf730df839eff60c01a14ef082a8
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-22 20:52:07 +0000

    added new prototype to header

--- monty.h
+++ monty.h
@@ -46,7 +46,6 @@ void free_stack(stack_t *head);
 int op_check(char *opcode, char *num);
 int monty_helper(unsigned int ln_count, char *line, stack_t **stack);
 int line_helper(FILE *fd, stack_t **stack);
-
 /*_____GLOBAL_VAR___________________________*/
 extern int num;
 
@@ -57,5 +56,6 @@ void op_push(stack_t **head, unsigned int n);
 int stack_init(stack_t **head, unsigned int n);
 void op_pall(stack_t **h, unsigned int n);
 void op_pint(stack_t **stack, unsigned int ln_count);
+void op_pop(stack_t **stack, unsigned int ln_count);
 
 #endif
