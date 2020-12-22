280ec9c0da512fd426bf6ddaee2fe1c4b3f4b479
ranicholson <ranicho2@gmail.com>
2020-12-21 19:46:03 -0600

    Updates

--- op_check.c
+++ op_check.c
@@ -9,25 +9,22 @@
 
 int op_check(char *opcode, char *num)
 {
-	int idx = 1, tmp = 0;
-	
+	int idx = 0, tmp = 0;
+
 	if (strcmp(opcode, "push") == 0)
 	{
 		if (num == NULL)
 			return (-1);
-
-		if (num[0] == '-' || num[0] == '+')
+		while (num[idx])
 		{
-			while (num[idx])
-			{
-				tmp = isdigit(num[idx]);
-				if (tmp == 0)
-					return (-1);
-				idx++;
-			}
+			if (idx == 0 && (num[idx] == '-' || num[idx] == '+'))
+				continue;
+
+			tmp = isdigit(num[idx]);
+			if (tmp == 0)
+				return (-1);
+			idx++;
 		}
-		else if (atoi(num) == 0 && num[0] != '0')
-			return (-1);
 	}
 
 	return (0);
