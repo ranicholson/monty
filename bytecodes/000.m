5508a4ecbd1b37edb40643cb166c6a7f4a9207d0
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-22 02:03:38 +0000

    added new changes to op_check

--- bytecodes/000.m
+++ bytecodes/000.m
@@ -1,14 +1,2 @@
-      pall 
-push -1
-push -2
-  push 3
-     push 30
-                   pall    
-push 4
-    push 5    
-      push    6    
-      push 60
-      pall
-      pall
-      push -10   
-pall 
+push 1
+pushe 2
--- op_check.c
+++ op_check.c
@@ -9,26 +9,22 @@
 
 int op_check(char *opcode, char *num)
 {
-	int idx = 1, tmp = 0;
-	
+	int idx = 0, tmp = 0;
+
 	if (strcmp(opcode, "push") == 0)
 	{
 		if (num == NULL)
 			return (-1);
 
 		if (num[0] == '-' || num[0] == '+')
+			idx++;
+		while(num[idx])
 		{
-			while (num[idx])
-			{
-				tmp = isdigit(num[idx]);
-				if (tmp == 0)
-					return (-1);
-				idx++;
-			}
+			tmp = isdigit(num[idx]);
+			if (tmp == 0)
+				return (-1);
+			idx++;
 		}
-		else if (atoi(num) == 0 && num[0] != '0')
-			return (-1);
 	}
-
 	return (0);
 }
